a9cbd0a81776e88aef3551be7f7a0c90a1c195b7
Brian Woodward <brian.woodward@gmail.com>
2016-07-29 15:54:33 -0400

    tests

--- test.js
+++ test.js
@@ -1,28 +1,44 @@
 'use strict';
 
 require('mocha');
+var groupArray = require('group-array');
 var assert = require('assert');
-var vinylGroup = require('./');
+var File = require('vinyl');
+var Base = require('base');
+var VinylGroup = require('./');
+var group, files;
 
 describe('vinyl-group', function() {
-      it('should export a function', function() {
-    assert.equal(typeof vinylGroup, 'function');
+  beforeEach(function() {
+    files = [
+      new File({path: 'one.hbs', data: {tags: ['foo']}, contents: new Buffer('')}),
+      new File({path: 'two.hbs', data: {tags: ['bar']}, contents: new Buffer('')}),
+      new File({path: 'three.hbs', data: {tags: ['foo']}, contents: new Buffer('')}),
+      new File({path: 'four.hbs', data: {tags: ['bar']}, contents: new Buffer('')}),
+      new File({path: 'five.hbs', data: {tags: ['foo', 'bar']}, contents: new Buffer('')}),
+    ];
+
+    group = groupArray(files, 'data.tags');
   });
 
-    it('should export an object', function() {
-    assert(vinylGroup);
-    assert.equal(typeof vinylGroup, 'object');
+  it('should export a function', function() {
+    assert.equal(typeof VinylGroup, 'function');
   });
 
-    it('should throw an error when invalid args are passed', function(cb) {
-    try {
-      vinylGroup();
-      cb(new Error('expected an error'));
-    } catch (err) {
-      assert(err);
-      assert.equal(err.message, 'expected first argument to be a string');
-      assert.equal(err.message, 'expected a callback function');
-      cb();
-    }
+  it('should create an instance', function() {
+    var vinylGroup = new VinylGroup();
+    assert(vinylGroup instanceof VinylGroup);
+    assert(vinylGroup instanceof Base);
+  });
+
+  it('should create an instance without new', function() {
+    var vinylGroup = VinylGroup();
+    assert(vinylGroup instanceof VinylGroup);
+    assert(vinylGroup instanceof Base);
+  });
+
+  it('should create an instance with a group', function() {
+    var vinylGroup = VinylGroup(group);
+    assert.deepEqual(vinylGroup.group, group);
   });
 });
